a6260b8912f6fdce7bc6b49b58e95284cc1f17c6
Eva <evaprigoghaeva@gmail.com>
2025-04-17 14:12:42 +0300

    add class ArrayD

--- src/main/java/by/itakadimy/boyarenko/ArrayMain.java
+++ src/main/java/by/itakadimy/boyarenko/ArrayMain.java
@@ -3,7 +3,7 @@ package by.itakadimy.boyarenko;
 public class ArrayMain {
     public static void main(String[] args) {
         int[] array;
-        int[] arrayInt = new int[100];
+        int[] arrayInt = new int[12];
         int[] amountDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         System.out.println("Апрель содержит " + amountDays[3] + " дней");
         for (int i = 0; i < amountDays.length; i++) {
