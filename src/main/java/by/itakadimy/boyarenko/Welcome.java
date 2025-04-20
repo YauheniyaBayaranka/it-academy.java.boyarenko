547198ef4dc9c5478f916a8e708a625ca3416952
Eva <evaprigoghaeva@gmail.com>
2025-04-20 14:00:35 +0300

    add class Welcome

---
+++ src/main/java/by/itakadimy/boyarenko/Welcome.java
@@ -0,0 +1,11 @@
+package by.itakadimy.boyarenko;
+
+public class Welcome {
+    public static String getWelcome() {
+        return "Welcome!";
+    }
+    public static void main(String[] args) {
+        String str = getWelcome();
+        System.out.println(str);
+    }
+}
